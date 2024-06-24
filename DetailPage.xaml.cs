813ea0a307b5ee66aa03bd8a24a9109414e5d76d
YipanWang <YipanWang@OTT-ENG-YIPANW>
2024-06-24 10:39:02 -0400

    Add project files.

---
+++ DetailPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiTodo.ViewModel;
+
+namespace MauiTodo
+{
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage(DetailViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTodo.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        public MainViewModel() {
+
+            Items = new ObservableCollection<string>();
+        
+        }
+        [ObservableProperty]
+        string text;
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+
+        [RelayCommand]
+        void Add()
+        {
+
+            if (string.IsNullOrEmpty(Text))
+            {
+                return;
+            }
+            Items.Add(Text);
+            Text = string.Empty;
+
+
+        }
+
+        [RelayCommand]
+        async Task Tap(string s)
+        {
+            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+        }
+    }
+}
